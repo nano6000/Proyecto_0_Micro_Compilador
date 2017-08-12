68d788b26a442d406306a2f8786fd337ec2fa1c5
Esteban Bosques <nano.95.eb@gmail.com>
2017-08-11 23:18:18 -0600

    Primeros archivos

---
+++ Tokens.h
@@ -0,0 +1,7 @@
+typedef enum token_types {
+		BEGIN, END, READ, WRITE, ID, INTLITERAL,
+		LPAREN, RPAREN, SEMICOLON, COMMA, ASSIGNOP,
+		PLUSOP, MINUSOP, SCANEOF 
+	} token;
+	
+extern token scanner(void);
