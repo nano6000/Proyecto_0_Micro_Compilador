75a1e08591be44de73f5d16e7664f26f8ea51bf0
Esteban Bosques <nano.95.eb@gmail.com>
2017-08-16 22:42:44 -0600

    parser.h

---
+++ Parser.h
@@ -0,0 +1,9 @@
+void system_goal (void);
+void program (void);
+void statement_list(void);
+void statement(void);
+void id_list (void);
+void expression(expr_rec *result);
+void expr_list(void);
+void add_op(void);
+void primary(void);
